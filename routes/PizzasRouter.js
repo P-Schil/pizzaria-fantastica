f7aeadc487389294670895a31083149bc32f6721
P-Schil <schil.ambiental@gmail.com>
2021-08-16 19:48:56 -0300

    adiconando pizzasRout.js do professor

--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -1,25 +1,20 @@
+  
 var express = require('express');
 var router = express.Router();
-const multer = require('multer')
-const armaznamento = multer.diskStorage({
-    destination :(req,file,cb)=>{
-        cb(null, '.public/img/')
-    },
-    filename: (req,file,cd)=>{
-        cb(null,Date.now()+ '-'+ file.originalname)
-    }
-})
+const VerificaUsuarioLogado = require("../middlewares/VerificaUsuarioLogado");
+const upload = require("../lib/upload");
 
 const PizzasController = require('../controllers/PizzasController');
-const uploader = require('');
 
 /* GET home page. */
 router.get('/', PizzasController.index);
-router.get('/pizzas/create',upload.single('img'), PizzasController.create)
-router.post('/pizzas/create', PizzasController.store)
+router.get('/busca', PizzasController.search);
+router.get('/pizzas/:id/edit', VerificaUsuarioLogado,PizzasController.edit);
+router.get('/pizzas/create', VerificaUsuarioLogado, PizzasController.create);
 router.get('/pizzas/:id', PizzasController.show);
-router.get('/busca/', PizzasController.busca);
-
-
+router.get('/pizzas', VerificaUsuarioLogado, PizzasController.list);
+router.post('/pizzas', VerificaUsuarioLogado, upload.single("img"), PizzasController.store);
+router.put('/pizzas/:id/update', VerificaUsuarioLogado, PizzasController.update);
+router.delete('/pizzas/:id', VerificaUsuarioLogado, PizzasController.delete);
 
 module.exports = router;
