01f0a7e7006c4ec28f8f0c5aa55bb751171cc314
P-Schil <schil.ambiental@gmail.com>
2021-08-16 20:05:47 -0300

    adicionando arqivo app.js da branch aula do professor

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var methodOveride = require('method-override');
-var session = require("express-session");
 
 var PizzasRouter = require('./routes/PizzasRouter');
-var AdmRouter = require('./routes/AdmRouter');
-var UsersRouter = require('./routes/UsersRouter');
+const LogIpHoraMiddleware = require('./middlewares/LogIpHoraMiddleware');
 
 var app = express();
 
@@ -19,22 +16,19 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({secret:"SEGREDO"}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOveride("_method"));
 
+app.use(LogIpHoraMiddleware);
 app.use('/', PizzasRouter);
-app.use('/', AdmRouter);
-app.use('/', UsersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
